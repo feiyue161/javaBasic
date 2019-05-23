6ddf5fe19cd623b667042db3821c13399492ee59
feiyue161 <gyf0528>
2019-05-23 20:03:40 +0800

    作业

--- src/com/neuedu/test/T.java
+++ src/com/neuedu/test/T.java
@@ -2,6 +2,6 @@ package com.neuedu.test;
 
 public class T {
     public static void main(String[] args) {
-        System.out.print(1);
+        System.out.print(153);
     }
 }
--- src/com/neuedu/test/Test.java
+++ src/com/neuedu/test/Test.java
@@ -2,6 +2,13 @@ package com.neuedu.test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.print(1);
+       int i=125;
+       int m=0;
+       while(i!=0){
+           m=i%10;
+           i=i/10;
+           System.out.print(m);
+
+       }
     }
 }
